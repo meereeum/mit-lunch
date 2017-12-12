45c3405392c736080f473b836c2953274839aaae
Miriam Shiffman <meereeum@gmail.com>
2017-12-12 17:06:34 -0500

    better date parsing

--- get_menu.py
+++ get_menu.py
@@ -7,22 +7,23 @@ from bs4 import BeautifulSoup
 from dateutil import parser
 
 
-def get_dishes(date="today", menu="Classic Cuisine"):
-    date = (datetime.now() if date is "today" else date)
-    date_str = "-".join([str(date.year), "{:02d}".format(date.month),
-                         "{:02d}".format(date.day)])
+def get_dishes(date='today', menu='Classic Cuisine'):
+    date = convert_date(date) # str -> datetime
 
-    BASE_URL = "http://mit.campusdish.com/Commerce/Catalog/Menus.aspx"
-    PARAMS = [("LocationId", "4932"), # Koch cafe
-              ("PeriodId", "1440"), # lunch
-              ("MenuDate", date_str),
-              ("Mode", "day")]
+    date_str = '-'.join([str(date.year), '{:02d}'.format(date.month),
+                            '{:02d}'.format(date.day)])
 
-    print("Reading the menu...")
-    soup = BeautifulSoup(requests.get(BASE_URL, PARAMS).content, "lxml")
+    BASE_URL = 'http://mit.campusdish.com/Commerce/Catalog/Menus.aspx'
+    PARAMS = [('LocationId', '4932'), # Koch cafe
+              ('PeriodId', '1440'), # lunch
+              ('MenuDate', date_str),
+              ('Mode', 'day')]
 
-    dishes = [dish.string for section in soup("div", class_="menu-details-station")
-              for dish in section("a", rel="prettyPhotoiFrameWithoutNavigation")
+    print('Reading the menu...')
+    soup = BeautifulSoup(requests.get(BASE_URL, PARAMS).content, 'lxml')
+
+    dishes = [dish.string for section in soup('div', class_='menu-details-station')
+              for dish in section('a', rel='prettyPhotoiFrameWithoutNavigation')
               if section.h2.string == menu]
 
     return dedupe(dishes)
@@ -33,18 +34,40 @@ def dedupe(lst):
     return OrderedDict((item, None) for item in lst).keys()
 
 
-if __name__ == "__main__":
-    kwargs = {}
-    try:
-        date = (datetime.now() + timedelta(days=1)
-                if sys.argv[1] in ("tomorrow", "tom")
-                else parser.parse(sys.argv[1]))
-        kwargs["date"] = date
-    except(IndexError):
-        pass # default to today
+def convert_date(date_in):
+    """Convert string to `datetime`"""
+    D_CONVERSIONS = {
+        'today': datetime.now(),
+        'tomorrow': datetime.now() + timedelta(days=1),
+        'tom': datetime.now() + timedelta(days=1),
+        'mon': 'monday',
+        'tues': 'tuesday',
+        'wed': 'wednesday',
+        'thurs': 'thursday',
+        'fri': 'friday'
+    }
+
+    try: # if abbrev, uncompress for parser
+        date_out = D_CONVERSIONS[date_in]
+    except(KeyError):
+        date_out = date_in
+
+    try: # if str, convert to datetime
+        date_out = parser.parse(date_out)
+    except(AttributeError):
+        date_out = date_out
     except(ValueError):
         print("I don't recognize that date.. try again ?")
         sys.exit(0)
 
+    return date_out
+
+if __name__ == '__main__':
+    kwargs = {}
+    try:
+        kwargs['date'] = sys.argv[1]
+    except(IndexError):
+        pass # default to today
+
     food = get_dishes(**kwargs)
-    print(""); print("__How about__"); print("\n".join(food)); print("")
+    print(''); print('__How about__'); print('\n'.join(food)); print('')
