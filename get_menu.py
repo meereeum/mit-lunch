8d097a904c7e2a5e6a5b9fa5239d2e32d497b2d2
Miriam Shiffman <meereeum@gmail.com>
2018-04-08 18:47:44 -0400

    simplify with unified pkg for CLIs

--- get_menu.py
+++ get_menu.py
@@ -1,15 +1,10 @@
-from collections import OrderedDict
-from datetime import datetime, timedelta
-import requests
 import sys
 
-from bs4 import BeautifulSoup
-from dateutil import parser
+from CLIppy import convert_date, dedupe, pprint_header_with_lines, soup_me
 
 
 def get_dishes(date='today', menu='Classic Cuisine'):
-    date = convert_date(date) # str -> datetime
-    date_str = date.strftime('%Y-%m-%d')
+    date_str = convert_date(date) # str -> datetime
 
     BASE_URL = 'http://mit.campusdish.com/Commerce/Catalog/Menus.aspx'
     PARAMS = [('LocationId', '4932'), # Koch cafe
@@ -18,45 +13,13 @@ def get_dishes(date='today', menu='Classic Cuisine'):
               ('Mode', 'day')]
 
     print('Reading the menu...')
-    soup = BeautifulSoup(requests.get(BASE_URL, PARAMS).content, 'lxml')
+    soup = soup_me(BASE_URL, PARAMS)
 
     menu_div = soup.find('div', attrs={'aria-label': menu})
     dishes = [dish.string for dish in menu_div('a', rel='prettyPhotoiFrameWithoutNavigation')]
 
     return dedupe(dishes)
 
-# via https://stackoverflow.com/questions/6197409/ordered-sets-python-2-7
-def dedupe(lst):
-    """list -> deduped iterable of items, in order"""
-    return OrderedDict((item, None) for item in lst).keys()
-
-def convert_date(date_in):
-    """Convert string to `datetime`"""
-    D_CONVERSIONS = {
-        'today': datetime.now(),
-        'tomorrow': datetime.now() + timedelta(days=1),
-        'tom': datetime.now() + timedelta(days=1),
-        'mon': 'monday',
-        'tues': 'tuesday',
-        'wed': 'wednesday',
-        'thurs': 'thursday',
-        'fri': 'friday'
-    }
-
-    try: # if abbrev, uncompress for parser
-        date_out = D_CONVERSIONS[date_in]
-    except(KeyError):
-        date_out = date_in
-
-    try: # if str, convert to datetime
-        date_out = parser.parse(date_out)
-    except(AttributeError, TypeError):
-        date_out = date_out
-    except(ValueError):
-        print("I don't recognize that date.. try again ?")
-        sys.exit(0)
-
-    return date_out
 
 if __name__ == '__main__':
     kwargs = {}
@@ -65,5 +28,7 @@ if __name__ == '__main__':
     except(IndexError):
         pass # default to today
 
+    header = 'How about'
     food = get_dishes(**kwargs)
-    print(''); print('__How about__'); print('\n'.join(food)); print('')
+    print(''); pprint_header_with_lines(header, food); print('')
+    #print(''); print('__How about__'); print('\n'.join(food)); print('')
